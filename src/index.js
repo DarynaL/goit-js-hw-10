07c04b29f4ab627d8c0e0c1276d57b17ae1b5e47
DarynaL <85280006+DarynaL@users.noreply.github.com>
2021-11-13 11:36:32 +0200

    добавлена шаблонизация

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './sass/main.scss';
+import './css/styles.css';
+import countryCardTpl from '../src/templates/country-info.hbs'
+
+const searchbox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const DEBOUNCE_DELAY = 300;
+
+
+searchbox.addEventListener('input', e => {
+    e.preventDefault();
+    const name = searchbox.value;
+    fetchCountries(name)
+        .then(renderCountryCard);
+});
+
+
+function renderCountryCard(name) {
+    const markup = countryCardTpl(name);
+    countryInfo.innerHTML = markup;
+        }
+
+function fetchCountries (name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => response.json())
+}
